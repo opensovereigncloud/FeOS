b082a216fab25242c3faf4704b9ee6876f1d349f
Malte Janduda <mail@janduda.net>
2024-02-24 23:08:08 +0100

    cleaning up build-container build (#8)

--- hack/build-container/make.mk
+++ hack/build-container/make.mk
@@ -1,4 +1,6 @@
 build-container:
-	cd hack/build-container && ./mk-build-container
+	docker rmi feos-builder
+	docker pull rust:1-bookworm
+	docker build hack/build-container -t feos-builder
 	mkdir -p target
 	touch hack/build-container
