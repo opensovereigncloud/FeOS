755d4217c6aa608379e55b20d6491110584b1388
Guvenc Gulce <guevenc.guelce@sap.com>
2025-10-23 15:36:44 +0100

    Fix release test to support container tests (#96)
    * Give higher timeout during test for the server to start
    
    Signed-off-by: Guvenc Gulce <guevenc.guelce@sap.com>
    
    * Fix release test
    
    Signed-off-by: Guvenc Gulce <guevenc.guelce@sap.com>
    
    ---------
    
    Signed-off-by: Guvenc Gulce <guevenc.guelce@sap.com>

--- feos/tests/integration/mod.rs
+++ feos/tests/integration/mod.rs
@@ -89,7 +89,7 @@ async fn setup_server() -> Arc<tokio::runtime::Runtime> {
     });
 
     info!("Waiting for the server to start...");
-    for _ in 0..30 {
+    for _ in 0..60 {
         if Channel::from_static(PUBLIC_SERVER_ADDRESS)
             .connect()
             .await
